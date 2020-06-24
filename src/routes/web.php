ac33d6a907c32cdc5dd68957b988de0f0649ccd9
Parthi <parthiban@uitouxsolutions.com>
2020-06-24 21:21:52 +0530

    bug fix

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,23 +1,19 @@
 <?php
 
-Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'vehicle-pkg'], function () {
+Route::group(['namespace' => 'Abs\VehiclePkg', 'middleware' => ['web', 'auth'], 'prefix' => 'vehicle-pkg'], function () {
 
-	
-		//Vehicle Make
-		Route::get('/vehicle-make/get-list', 'VehicleMakeController@getVehicleMakeList')->name('getVehicleMakeList');
-		Route::get('/vehicle-make/get-form-data', 'VehicleMakeController@getVehicleMakeFormData')->name('getVehicleMakeFormData');
-		Route::post('/vehicle-make/save', 'VehicleMakeController@saveVehicleMake')->name('saveVehicleMake');
-		Route::get('/vehicle-make/delete', 'VehicleMakeController@deleteVehicleMake')->name('deleteVehicleMake');
-		Route::get('/vehicle-make/get-filter-data', 'VehicleMakeController@getVehicleMakeFilterData')->name('getVehicleMakeFilterData');
+	//Vehicle Make
+	Route::get('/vehicle-make/get-list', 'VehicleMakeController@getVehicleMakeList')->name('getVehicleMakeList');
+	Route::get('/vehicle-make/get-form-data', 'VehicleMakeController@getVehicleMakeFormData')->name('getVehicleMakeFormData');
+	Route::post('/vehicle-make/save', 'VehicleMakeController@saveVehicleMake')->name('saveVehicleMake');
+	Route::get('/vehicle-make/delete', 'VehicleMakeController@deleteVehicleMake')->name('deleteVehicleMake');
+	Route::get('/vehicle-make/get-filter-data', 'VehicleMakeController@getVehicleMakeFilterData')->name('getVehicleMakeFilterData');
 
-		
-		//Vehicle Model
-		Route::get('/vehicle-model/get-list', 'VehicleModelController@getVehicleModelList')->name('getVehicleModelList');
-		Route::get('/vehicle-model/get-form-data', 'VehicleModelController@getVehicleModelFormData')->name('getVehicleModelFormData');
-		Route::post('/vehicle-model/save', 'VehicleModelController@saveVehicleModel')->name('saveVehicleModel');
-		Route::get('/vehicle-model/delete', 'VehicleModelController@deleteVehicleModel')->name('deleteVehicleModel');
-		Route::get('/vehicle-model/get-filter-data', 'VehicleModelController@getVehicleModelFilterData')->name('getVehicleModelFilterData');
-
-		
+	//Vehicle Model
+	Route::get('/vehicle-model/get-list', 'VehicleModelController@getVehicleModelList')->name('getVehicleModelList');
+	Route::get('/vehicle-model/get-form-data', 'VehicleModelController@getVehicleModelFormData')->name('getVehicleModelFormData');
+	Route::post('/vehicle-model/save', 'VehicleModelController@saveVehicleModel')->name('saveVehicleModel');
+	Route::get('/vehicle-model/delete', 'VehicleModelController@deleteVehicleModel')->name('deleteVehicleModel');
+	Route::get('/vehicle-model/get-filter-data', 'VehicleModelController@getVehicleModelFilterData')->name('getVehicleModelFilterData');
 
 });
