08e8c11145ed90f8c69bf23aca369fdd3a649e11
Ram <ramakrishnan@uitoux.in>
2020-06-29 20:07:35 +0530

    Update VehicleModel.php

--- src/models/VehicleModel.php
+++ src/models/VehicleModel.php
@@ -5,6 +5,7 @@ namespace Abs\VehiclePkg;
 use Abs\HelperPkg\Traits\SeederTrait;
 use App\Company;
 use App\Config;
+use Auth;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
